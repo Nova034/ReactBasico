8ccfc568be3606cdbe8c507f31f256fa486084e4
Nova034 <jcmg0304@gmail.com>
2024-07-22 19:53:20 -0400

    Componente de Twitter

--- proyectos/reactPrueba/src/App.jsx
+++ proyectos/reactPrueba/src/App.jsx
@@ -1,6 +1,44 @@
+import { TwitterCard } from "./TwitterCard/TwitterCard";
+import "./App.css"
+
 function App () {
     return (
-        <div>Twitter Card</div>
+        <section className="App">
+            <TwitterCard 
+            siguiendo={false} 
+            nombreUsuario='Elon Musk' 
+            usuario='ElonMusk' /> 
+
+            <TwitterCard 
+            siguiendo 
+            nombreUsuario='Gacha Memes' 
+            usuario='GenshinImpactOC' /> 
+
+            <TwitterCard 
+            siguiendo={false} 
+            nombreUsuario='Keitaro' 
+            
+            usuario='DqKeita' /> 
+
+            <TwitterCard 
+            siguiendo nombreUsuario='Miguel Ángel Durán' 
+            usuario='midudev' /> 
+
+            <TwitterCard 
+            siguiendo 
+            nombreUsuario='Cosmic Red (dark mode)' 
+            usuario='carmenansio' /> 
+
+            <TwitterCard 
+            siguiendo 
+            nombreUsuario='Putupau' 
+            usuario='PutuPauu' /> 
+            
+            <TwitterCard 
+            siguiendo 
+            nombreUsuario='⚡SkyZero⚡' 
+            usuario='XSkyZeroX' /> 
+        </section>
     )
 }
 
